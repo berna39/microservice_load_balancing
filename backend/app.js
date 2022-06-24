67104fa3d3a593a94e2609ea026d35bb8867bc32
berna39 <kalemajoseph39@gmail.com>
2022-06-24 20:17:35 +0200

    Patch proxy

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 
 const appid = process.env.APPID
-const timeouts = [20000, 10000, 15000];
-const timeout =  Math.floor(Math.random() * timeouts.length);
+// const timeouts = [20000, 10000, 15000];
+// const timeout =  Math.floor(Math.random() * timeouts.length);
 
 app.get('/', (req, res) => {
     res.send(`Hello from ${appid}`);
@@ -13,12 +13,12 @@ const server = app.listen(appid, (err) => {
     console.log(`Termiator Server running on ${appid}`);
 });
 
-console.log(`Timeout ${timeouts[timeout]}`)
+// console.log(`Timeout ${timeouts[timeout]}`)
 
-setTimeout(()=>{
-    console.log(`Received kill signal, shutting down gracefully`);
-    server.close(() => {
-        console.log(`Closed out connection of ${appid}`);
-        process.exit(0);
-    });
-}, timeouts[timeout]);
+// setTimeout(()=>{
+//     console.log(`Received kill signal, shutting down gracefully`);
+//     server.close(() => {
+//         console.log(`Closed out connection of ${appid}`);
+//         process.exit(0);
+//     });
+// }, timeouts[timeout]);
