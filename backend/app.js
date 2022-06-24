4ba2468410804553c3d66bf4ef7139d31c9be30e
berna39 <kalemajoseph39@gmail.com>
2022-06-24 14:04:47 +0200

    Round robin demo

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,23 @@ const express = require("express");
 const app = express();
 
 const appid = process.env.APPID
+const timeouts = [20000, 10000, 15000];
+const timeout =  Math.floor(Math.random() * timeouts.length);
 
 app.get('/', (req, res) => {
     res.send(`Hello from ${appid}`);
 });
 
-app.listen(appid, (err) => {
+const server = app.listen(appid, (err) => {
     console.log(`Termiator Server running on ${appid}`);
 }); 
+
+console.log(`Timeout ${timeouts[timeout]}`)
+
+setTimeout(()=>{
+    console.log(`Received kill signal, shutting down gracefully`);
+    server.close(() => {
+        console.log(`Closed out connection of ${appid}`);
+        process.exit(0);
+    });
+}, timeouts[timeout]);
